a7fab2258d46557806731b360e9502e40490dbc3
rbfl6418 <rbfl6418@gmail.com>
2021-07-13 00:36:20 +0900

    Update notion_to_github_pages.sh

--- notion_to_github_pages.sh
+++ notion_to_github_pages.sh
@@ -101,8 +101,8 @@ for exported_foldername in ${exported_foldername_array[*]}; do
     
     # Changing a image path in exported_filename.md
     exported_filename_for_images_path=$(echo "$exported_filename" | sed 's/ /%20/g') # 파일명에 공백있는 경우: %20으로 수정. 추후 md 내 이미지 경로에 이용
-    sed -i '' "s|"$exported_filename_for_images_path"/Untitled|/$images_folder_path/$fixed_filename2/Untitled|g" "$exported_file_path"
-    # sed -i "s|"$exported_filename_for_images_path"/Untitled|/$images_folder_path/$fixed_filename2/Untitled|g" "$exported_file_path"
+#     sed -i '' "s|"$exported_filename_for_images_path"/Untitled|/$images_folder_path/$fixed_filename2/Untitled|g" "$exported_file_path"
+    sed -i "s|"$exported_filename_for_images_path"/Untitled|/$images_folder_path/$fixed_filename2/Untitled|g" "$exported_file_path"
 
 
     # Changing a file name and move
